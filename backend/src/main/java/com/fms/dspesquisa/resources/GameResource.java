850428e9d6aa676cfa19ff573ea1ca4a88de0bf3
FMS0102 <felsimoes@gmail.com>
2020-09-15 01:31:06 -0300

    DTO, Service

---
+++ backend/src/main/java/com/fms/dspesquisa/resources/GameResource.java
@@ -0,0 +1,26 @@
+package com.fms.dspesquisa.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fms.dspesquisa.dto.GameDTO;
+import com.fms.dspesquisa.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameResource {
+
+	@Autowired
+	private GameService service;
+
+	@GetMapping
+	public ResponseEntity<List<GameDTO>> findAll() {
+		List<GameDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
